8b96212f9182b9a7fc128729cf2984e9fac92365
dtulumidis <dextavision@gmail.com>
2021-02-17 16:29:40 +0100

    created basic structure of Weathy

---
+++ lib/app/app.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:weathy/home/home_screen.dart';
+
+class Weathy extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomeScreen(),
+    );
+  }
+}
